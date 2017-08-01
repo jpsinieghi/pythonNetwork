f7aa4d50a89f2c9ad8215563046d8385b7b9c152
jpsinieghi <jpsinieghi@gmail.com>
2017-08-01 10:26:23 -0300

    Escuta Ping v1

--- identifica_ping_IP.py
+++ identifica_ping_IP.py
@@ -12,7 +12,6 @@ class EscutaPing:
     def escuta(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
         s.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
-        print s
 
         while 1:
             data, addr = s.recvfrom(1508)
